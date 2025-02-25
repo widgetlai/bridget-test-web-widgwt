365a065e411cce1f27e07b0187b057c636155de5
widgetlai <bridgetwlai@gmail.com>
2025-02-25 04:05:19 -0500

    cleaned up blog page

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,11 @@
 // This is where images etc get called in from /public/images //
 ////////////////////////////////////////////////////////////////
 
-import { useState, useEffect } from "react"
-import { usePathname } from "next/navigation"
-import ImageViewer from "./components/ImageViewer"
-import { getAllImages } from "./utils/images"
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="w-full flex justify-center pt-10 pb-20">
+    <div className="min-h-screen flex items-center justify-center -mt-32">
       <Image
         src="/images/homepage_wheat.jpg"
         alt="homepage wheat"
