9cbc270093562103e6dfd2fe8e0fecd402f0a839
widgetlai <bridgetwlai@gmail.com>
2025-02-26 01:02:14 -0500

    minor tweaks
    made top margin of text consistent w that of entropy page, updated images to 72dpi on DE preview and landing page

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -2,9 +2,9 @@
 
 export default function InfoPage() {
   return (
-    <div className="max-w-[1400px] mx-auto mt-[30px]">
+    <div className="max-w-[1400px] mx-auto mt-16 sm:mt-24 lg:mt-32">
       
-      <section className="max-w-[50%] space-y-4 text-xs">
+      <section className="max-w-[50%] space-y-4 text-base text-light">
         <p>
           Bridget Lai (b. 2000, Minneapolis, MN) is a photographer based in New York whose work is primarily collaged self-portraiture in which she assumes the role of a girl between modes of work and play. Her pieces ask how coding labor via cuteness can affect its visibility, and how entropy is a variable in its relationship. In 2022, Lai received her BFA in Photography from Parsons School of Design.
         </p>
