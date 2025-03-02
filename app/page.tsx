e40c8a5902fd99862e7fe9acbe5065a970363767
widgetlai <bridgetwlai@gmail.com>
2025-03-01 22:59:22 -0500

    updated homepage

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,13 @@
 // This is where images etc get called in from /public/images //
 ////////////////////////////////////////////////////////////////
 
+
 import Image from 'next/image'
 
+
 export default function Home() {
+
+
   return (
     <div className="mt-24 mb-24 fixed left-1/2 top-28 -translate-x-1/2 flex flex-col items-center justify-center">
       <Image
