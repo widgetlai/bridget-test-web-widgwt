8cbc1e27a00a52d1db645897e77a8e8d3fb4ad3d
widgetlai <bridgetwlai@gmail.com>
2025-03-01 23:07:56 -0500

    changed case sensitive image name, momo now visible.

--- app/momo/page.tsx
+++ app/momo/page.tsx
@@ -6,7 +6,13 @@ export default function MomoPage() {
   return (
     <div>
       <div className="flex items-center justify-center mb-14 lg:mb-16 sm:mb-12 mt-22 lg:mt-24 sm:mt-20">
-        <Image src="/images/momo/momo_01.jpg" alt="Momo" width={600} height={800} />
+        <Image 
+        src="/images/momo/momo_01.JPG" 
+        alt="Momo" 
+        width={600} 
+        height={800} 
+        priority
+        />
       </div>
 
       <section className="flex flex-col items-center justify-center text-xs text-light text-center">
